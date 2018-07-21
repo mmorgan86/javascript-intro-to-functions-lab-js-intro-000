f0c399555c41df6a1769f1f6e47dadbf9dad80c2
Michael Morgan <michael1986morgan@gmail.com>
2018-07-21 04:48:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,22 +31,4 @@ let sayHiToGrandma = (string) => {
   }
 }
 
-// let sayHiToGrandma = (string) => {
-//     switch (string) {
-//     case (string === string.toLowerCase()):  
-//       return "I can't hear you!"
-//       break;
-//     case (string === string.toUpperCase()):
-//       return "YES INDEED!"
-//       break;
-//     case (string === "I love you, Grandma."):
-//       return "I love you, too."
-//       break;
-//   }
-// }
-
-// sayHiToGrandma
-
-
-
-
+sayHiToGrandma
