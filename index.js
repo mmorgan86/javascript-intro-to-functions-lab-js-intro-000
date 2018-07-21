5a06749029cd1fd73d02055b84ca388b35eddc25
Michael Morgan <michael1986morgan@gmail.com>
2018-07-21 04:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ logWhisper
 
 let sayHiToGrandma = (string) => {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+      return "I can't hear you!"
   } else if (string === string.toUpperCase()) {
-    return "YES INDEED!"
-  } else if (string === string.toLowerCase()) {
-    
+      return "YES INDEED!"
+  } else if (string === "I love you, Grandma." {
+      return "I love you, too."
   }
 }
 
